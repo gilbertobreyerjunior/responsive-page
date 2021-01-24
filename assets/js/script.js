365809ac1dcae3b563092af557ac0019d6e7f74d
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2021-01-24 15:40:27 -0300

    Implementando em JavaScript abrir e fechar do menu responsivo

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.querySelector('.open-menu').addEventListener('click', e=> {
+
+
+    document.querySelector('header .menu').classList.add('open');
+
+        });
+
+     document.querySelector('.close-menu').addEventListener('click', e=> {
+
+        document.querySelector('header .menu').classList.remove('open');
+
+                });
+
+                
